b2d78113f7929073cadb0f51e7c2f86269a69d2c
ShikhaPallavi123 <157258801+ShikhaPallavi123@users.noreply.github.com>
2025-02-20 12:26:38 -0800

    Time: 4 ms (90.03%) | Memory: 23.6 MB (52.99%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.cpp
@@ -1,42 +1,39 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        // Arrays to store frequency of each character
         int count1[26] = {0};
         int count2[26] = {0};
 
-        // Check if lengths are different
-        if (word1.size() != word2.size()) {
+        if(word1.size() != word2.size()){
             return false;
         }
 
-        // Count frequency of characters in word1
-        for (char c : word1) {
-            count1[c - 'a']++;
+        for(int i = 0; i < word1.size(); i++){
+            int index1 = word1[i] -'a';
+            count1[index1]++;
+        }
+        for(int i = 0; i < word2.size(); i++){
+            int index2 = word2[i] -'a';
+            count2[index2]++;
         }
 
-        // Count frequency of characters in word2
-        for (char c : word2) {
-            count2[c - 'a']++;
+        for(int i = 0; i < 26; i++) {
+            if((count1[i] != 0 && count2[i] == 0 ) || (count1[i] == 0 && count2[i] != 0 )){
+                return false;
+
+            }
+            
         }
 
-        // Check if both strings have the same set of characters
-        for (int i = 0; i < 26; i++) {
-            if ((count1[i] == 0 && count2[i] != 0) || (count1[i] != 0 && count2[i] == 0)) {
+
+        sort(count1, count1+26);
+        sort(count2, count2+26);
+
+        for(int i = 0; i < 26; i++){
+            if(count1[i] != count2[i]){
                 return false;
             }
         }
-
-        // Sort frequency counts and compare
-        sort(count1, count1 + 26);
-        sort(count2, count2 + 26);
-
-        for (int i = 0; i < 26; i++) {
-            if (count1[i] != count2[i]) {
-                return false;
-            }
-        }
-
         return true;
     }
 };
