4fb8e3e6ff7127d0483144d25b2d468df59cb03a
ShikhaPallavi123 <157258801+ShikhaPallavi123@users.noreply.github.com>
2025-05-06 13:12:43 -0700

    Time: 2 ms (77.38%) | Memory: 14.8 MB (50.01%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -6,7 +6,7 @@ public:
         for(int i = 0; i < nums.size(); i++){
             int complement = target - nums[i];
             if(seen.find(complement) != seen.end()){
-                return{(seen[complement]), i};
+                return{seen[complement], i};
             }
 
             seen[nums[i]] = i;
